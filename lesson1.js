4fe8ae3874e5d229d432b7c565bfdb9ee389242f
antoninapole <9233007373@mail.ru>
2021-02-22 16:39:57 +0700

    lesson1
    lesson1

---
+++ lesson1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// задача №1
+
+var Tc = 30;
+var Tf = (9 / 5) * Tc + 32;
+console.log(Tf);
+
+// задача №2
+
+var name = 'Василий';
+var admin = name;
+console.log(admin);
+
+// задача №3 - 1000 + "108" = 1000108
+
